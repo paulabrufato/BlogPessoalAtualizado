dba9016de9a9c5eb2353d2f27c791a3255a89811
paulabrufato <paulabrufato.pb@gmail.com>
2022-06-01 11:06:00 -0300

    @paulabrufato [Task: Modelos, Repositórios e Controladores]{01/06/2022 | 11:05}

---
+++ BlogPessoal/src/repositorios/ITema.cs
@@ -0,0 +1,22 @@
+﻿using BlogPessoal.src.modelos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlogPessoal.src.repositorios
+{
+    /// <summary>
+    /// <para>Resumo: Responsavel por representar ações de CRUD de tema</para>
+    /// <para>Criado por: Ana Paula Rivera Brufato</para>
+    /// <para>Versão: 1.0</para>
+    /// <para>Data: 01/06/2022</para>
+    /// </summary>
+    public interface ITema
+    {
+        Task<List<Tema>> PegarTodosTemasAsync();
+        Task<Tema> PegarTemaPeloIdAsync(int id);
+        Task<List<Tema>> PegarTemasPelaDescricaoAsync(string descricao);
+        Task NovoTemaAsync(Tema tema);
+        Task AtualizarTemaAsync(Tema tema);
+        Task DeletarTemaAsync(int id);
+    }
+}
---
+++ BlogPessoal/src/servicos/IAutenticacao.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using BlogPessoal.src.modelos;
+
+namespace BlogPessoal.src.servicos
+{
+    /// <summary>
+    /// <para>Resumo: Interface Responsavel por representar ações de autenticação</para>
+    /// <para>Criado por: Ana Paula Rivera Brufato</para>
+    /// <para>Versão: 1.0</para>
+    /// <para>Data: 01/06/2022</para>
+    /// </summary>
+    public interface IAutenticacao
+    {
+        string CodificarSenha(string senha);
+        Task CriarUsuarioSemDuplicarAsync(Usuario usuario);
+        string GerarToken(Usuario usuario);
+    }
+}
